f2385e3020eb88323a59ff3bfb219e6c7199c17e
AshrafHabromman <ashraf.habrumman@gmail.com>
2023-07-31 16:13:46 +0300

    Add models to build the tables from them

---
+++ DoctorWho/DoctorWho.DB/Models/Episode.cs
@@ -0,0 +1,17 @@
+﻿using DoctorWho.DB.Enums;
+
+namespace DoctorWho.DB.Models
+{
+    public class Episode
+    {
+        public int Id { get; set; }
+        public int SeriesNumber { get; set; }
+        public int EpisodeNumber { get; set; }
+        public EpisodeType EpisodeType { get; set; } = EpisodeType.Regular;
+        public string Title { get; set; } = string.Empty;
+        public DateTime EpisodeDate { get; set; }
+        public int AuthorId { get; set; }
+        public int DoctorId { get; set; }
+        public string Notes { get; set; } = string.Empty;
+    }
+}
