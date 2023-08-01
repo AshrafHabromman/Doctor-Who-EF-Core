6de97ca72f5b1e81be4ffb6562fa394f53a84de6
AshrafHabromman <ashraf.habrumman@gmail.com>
2023-08-02 01:39:15 +0300

    Edit the migration files and enhance them

---
+++ DoctorWho/DoctorWho.DB/Migrations/20230801214757_CreateFunctionsMigration.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DoctorWho.DB.Migrations
+{
+    public partial class CreateFunctionsMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"CREATE FUNCTION dbo.fnEnemies(@EpisodeId INT)
+                                    RETURNS VARCHAR(255)
+                                    AS
+                                    BEGIN 
+	                                    DECLARE @enemies VARCHAR(255)
+	                                    SET @enemies = ''
+	                                    SELECT @enemies = COALESCE(@enemies + ', ', '') +  E.Name 
+	                                    FROM Enemies E
+	                                    WHERE E.Id IN (
+		                                    SELECT DISTINCT EE.EnemyId
+		                                    FROM EpisodeEnemy EE
+		                                    WHERE EE.EpisodeId = @EpisodeId
+	                                    )
+
+	                                    RETURN @enemies
+                                    END");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DROP FUNCTION dbo.fnEnemies;");
+        }
+    }
+}
---
+++ DoctorWho/DoctorWho.DB/Migrations/20230801215310_CreateViewEpisodes.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DoctorWho.DB.Migrations
+{
+    public partial class CreateViewEpisodes : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"CREATE VIEW [dbo].[viewEpisodes] AS 
+                                    SELECT  A.Name, D.Name,
+                                    dbo.fnCompanions(E.Id) AS CompanionsNames, 
+                                    dbo.fnEnemies(E.Id) AS EnemiesNames
+
+                                    FROM Episodes E 
+                                    INNER JOIN Authors A on E.AuthorId = A.Id
+                                    INNER JOIN Doctors D on E.DoctorId = D.Id
+                                ");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DROP VIEW dbo.viewEpisodes");
+        }
+    }
+}
