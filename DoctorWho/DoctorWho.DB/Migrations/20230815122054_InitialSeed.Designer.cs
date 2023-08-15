2a96176ba02514048a19806036cdb81df7a2b4f8
AshrafHabromman <ashraf.habrumman@gmail.com>
2023-08-15 18:21:27 +0300

    Create InitialSeed migration to insert the data into database

--- DoctorWho/DoctorWho.DB/Migrations/20230815122054_InitialSeed.Designer.cs
+++ DoctorWho/DoctorWho.DB/Migrations/20230815122054_InitialSeed.Designer.cs
@@ -4,6 +4,7 @@ using DoctorWho;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace DoctorWho.DB.Migrations
 {
     [DbContext(typeof(DoctorWhoCoreDBContext))]
-    partial class DoctorWhoCoreDBContextModelSnapshot : ModelSnapshot
+    [Migration("20230815122054_InitialSeed")]
+    partial class InitialSeed
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
